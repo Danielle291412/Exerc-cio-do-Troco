8e21747e3f19a10ee483e20893b6def849e1ebf1
Danielle Gouveia <danielle.vieira9@etec.sp.gov.br>
2024-09-08 17:46:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+decimal valorCompra, valorPago, valorTroco;
+
+Console.WriteLine("--- Troco ---\n");
+
+Console.Write("Valor da compra (R$)...:");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor pago (R$)........: ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+
+valorTroco = valorPago - valorCompra;
+
+Console.WriteLine($"\nTroco: {valorTroco:C}");
